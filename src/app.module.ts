25829360c9f6ff5ceb105f8115d93765fc17f26d
leoggarcia <leogjli66@gmail.com>
2025-11-03 16:39:25 -0600

    Typeorn config and user entity creation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db.sqlite',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true, // only in dev
+    }),
+  ],
   controllers: [],
   providers: [],
 })
